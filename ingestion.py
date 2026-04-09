093efa66cf246bf0eef086cd37463076f662fe27
abhinavsingh-hub <abhinavsinghdhakrekm@gmail.com>
2026-04-09 13:08:16 +0530

    ingestion pipeline

---
+++ ingestion.py
@@ -0,0 +1,23 @@
+def loading_documents(doc_path="docs"):
+    """Loading documents here
+    """
+
+    print(f"Loading documents from {doc_path}")
+    
+
+
+
+def main():
+    print("Ingestion Pipeline started...")
+
+    #Loading Documents
+
+    #Chunking
+
+    #Embeddings
+
+    #Store in vector DB
+
+    print("Ingestion Pipeline completed...")
+if __name__ == "__main__":
+    main()
