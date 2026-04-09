eb4549c8febd567bf13ac326d96537625dddf404
abhinavsingh-hub <abhinavsinghdhakrekm@gmail.com>
2026-04-09 13:35:32 +0530

    Ingestion completed

--- ingestion.py
+++ ingestion.py
@@ -1,9 +1,38 @@
+import os
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
+
 def loading_documents(doc_path="docs"):
     """Loading documents here
     """
 
     print(f"Loading documents from {doc_path}")
+
+    # if directory dosent exist
+    if not os.path.exists(doc_path):
+        raise FileNotFoundError(f"Directory {doc_path} not found")
+
+    # if file dosent exist
+    loader = DirectoryLoader(
+        doc_path, 
+        glob="**/*.txt", 
+        loader_cls=TextLoader)
     
+    documents = loader.load()
+    
+    if len(documents)==0:
+        raise ValueError(f"No txt files exist in {doc_path} folder")
+
+    for i, doc in enumerate(documents):
+        print(f" --- Document {i+1} --- ")
+        print(f" Source: {doc.metadata['source']}")
+        print(f" Length: {len(doc.page_content)} characters")
+        print(f" Content: {doc.page_content}")
+        print("\n")
+
+    return documents
+    
+    
+   
 
 
 
